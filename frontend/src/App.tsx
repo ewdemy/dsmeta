7375fae7f32a840758f91d362a716bc9e4c4f58f
ewdemy <ewdemy@hotmail.com>
2022-11-16 00:32:13 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
-
+import NotificationButton from './components/NotificationButton'
 function App() {
   
   return (
     <>
       <h1>Hello from react!</h1>
+      <NotificationButton/>
     </>
   )
 }
