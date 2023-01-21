558decb39a819ff7882e366112bf08e4f6718ff6
ewdemy <ewdemy@hotmail.com>
2023-01-21 13:39:06 -0300

    send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
 import icon from "../../assets/img/notification-icon.svg";
 import "./styles.css";
-function Notificationbutton() {
+import axios from 'axios';
+import { BASE_URL } from './../../utils/request';
+type Props = {
+  saleId: number;
+}
+
+function handleClick(id:number) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(resp => {
+      alert("FUNFOU")
+    })
+}
+
+function Notificationbutton({saleId}: Props) {
   return (
     <>
-      <div className="dsmeta-red-btn">
+      <div className="dsmeta-red-btn" onClick={()=> handleClick(saleId)} >
         <img src={icon} alt="notificar" />
       </div>
     </>
